980bb104296da51b5c32541a59d50f2a292cff3e
Gaebuidhe <fraziejr@email.sc.edu>
2017-11-13 00:01:39 -0500

    b.2

--- RealmAndCharts-example/RealmAndCharts-example/ViewController.swift
+++ RealmAndCharts-example/RealmAndCharts-example/ViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var txtTextBox: UITextField!
+    @IBOutlet weak var chtChart: LineChartView!
+    
+    var numbers: [Double] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnbutton(_ sender: Any) {
+        
+        let input = Double(txtTextBox.text!)
+        
+        numbers.append(input!)
+        updateGraph()
+    }
+    
+    func updateGraph(){
+        var lineChartEntry = [ChartDataEntry]()
+        
+        for i in 0..<numbers.count{
+            let value = ChartDataEntry(x: Double(i), y: numbers[i])
+            lineChartEntry.append(value)
+        }
+        
+        let line1 = LineChartDataSet(values: lineChartEntry, label: "Number")
+        
+        line1.colors = [NSUIColor.blue]
+        
+        let data = LineChartData()
+        
+        data.addDataSet(line1)
+        
+        chtChart.data = data
+        
+        chtChart.chartDescription?.text = "My awesome chart"
+    }
+    
 }
 
